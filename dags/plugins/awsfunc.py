cbbc5601f128bf28854f7286d38250d079f82ddf
namuna309 <nyah309@gmail.com>
2023-08-31 16:16:20 +0900

    stakcoverflow 질문 수집 dag 생성

--- dags/plugins/awsfunc.py
+++ dags/plugins/awsfunc.py
@@ -1,17 +1,18 @@
+# -*- coding: utf-8 -*-
 import json
-from airflow.models import Variable
 import boto3
+import botocore
+from datetime import datetime, timedelta
 from botocore.exceptions import NoCredentialsError
+import pandas as pd
 
 class awsfunc:
     def __init__(self, service_name):
         # Boto3 클라이언트 생성 (IAM 역할을 사용)
         session = boto3.session.Session()
         self.client = session.client(
-            aws_access_key_id=Variable.get('aws_access_key_id'),
-            aws_secret_access_key=Variable.get('aws_secret_access_key'),
             service_name=service_name,
-            region_name="ap-northeast-2"
+            region_name="us-east-1"
         )
 
     def getapikey(self, secret_id):
@@ -35,6 +36,21 @@ class awsfunc:
         except NoCredentialsError:
             print("No AWS credentials found.")
     
-    def ec2tos3(self, Body, Bucket, Key):
+    def ec2tos3(self, Body, Bucket, Path):
         # ec2에서 추출한 데이터 s3로 write
-        self.client.put_object(Body=Body, Bucket=Bucket, Key=Key)
+        self.client.put_object(Body=Body, Bucket=Bucket, Key=Path)
+
+    def read_json_from_s3(self, Bucket, Path):
+        response = self.client.get_object(Bucket=Bucket, Key=Path)
+        content = response["Body"]
+        jsonObject = json.loads(content.read().strip())
+        return jsonObject
+    
+    def get_file_name_from_s3(self, Bucket, Path):
+        objects = self.client.list_objects(Bucket=Bucket, Prefix = Path)
+        contents = objects.get('Contents', [])
+        last_content = contents[-1]
+        file_path = last_content['Key']
+        return file_path
+            
+            
